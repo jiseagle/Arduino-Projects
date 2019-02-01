7f98835a2231adcb63f19fdae0abd379940e6bca
jiseagle <eagle.norman6555@gmail.com>
2019-02-01 17:41:53 +0800

    temperary @20190201

--- Norn_DIY/Norn_Jan28b/Norn_moves/Norn_moves.ino
+++ Norn_DIY/Norn_Jan28b/Norn_moves/Norn_moves.ino
@@ -5,7 +5,8 @@
 #include <Oscillator.h>
 #include <Norn.h>
 #include <UltraSonicSensor.h>
-//#include <EEPROM.h>
+#include <NornSing.h>
+//#include <EEPROM.h>             //Remove annotation if you need to write trim value to EEPOM
 
 //-- First Step: Configure the Servo Pin
 /*
@@ -28,24 +29,27 @@
 #define PIN_RR            5     // define pin for servo[3]
 #define TrigPin           6     // define Ultra Sonic Sensor Trig Pin
 #define EchoPin           7     // define Ultra Sonic Sensor Echo Pin
+#define SpkPin           11     // define speaker out pin
 
-Norn Norn;  //Creat a Norn object
+Norn Norn;                      //Creat a Norn object
 int c;
 
+
 void setup() {
-  Norn.init(PIN_YL, PIN_YR, PIN_RL, PIN_RR,true, TrigPin, EchoPin); // Init Norn Robot, 
-                                                  // 5th argument is true to load Servo calibrated trim values
-  Serial.begin(9600);                           // remove annotation while you need serial port.
-  //Norn.setTrims(-10,2,0,-5);                     // Try calibrated trim manually if needed, then remove annotation mark
-  //Norn.saveTrimsOnEEPROM();                     // Only execute once while you confirm trim values to avoid EEPROM damaged
-  
+  // Init Norn Robot.
+  // 5th argument is true to load Servo calibrated trim values
+  Norn.init(PIN_YL, PIN_YR, PIN_RL, PIN_RR, true, TrigPin, EchoPin, SpkPin); 
+                                                                    
+  //sing.init(11);  
+  //sing.RobotOn();                                                                  
+  Serial.begin(9600);                                               // remove annotation while you need serial port.
+  //Norn.setTrims(-10,2,0,-5);                                      // Try calibrated trim manually if needed, then remove annotation mark
+  //Norn.saveTrimsOnEEPROM();                                       // Only execute once while you confirm trim values to avoid EEPROM damaged
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //Norn.walk(2,2000,1);   //Norn.walk*=(steps, T=periods, directions)
-  //Norn.Turn(600, 1600, 1, -1);  //Norn.Turn(steps, T=periods, turn, directions)
-  //Norn.resetServos();
+
   c = Serial.read();
   switch(c){
     case (48):
@@ -54,15 +58,16 @@ void loop() {
       break;
 
     case(49):
-      //Avoidance();
-      Serial.println(Norn.getDistance());
+
       break;
 
     case(50):
-      int A[4]= {0, 0, 30, 30};
-      int O[4] = {0, 0, 30, -30};
-      double phase_diff[4] = {0, 0, 0, 0};
-      Norn.execution(A,O,800,phase_diff, 2);
+      int h=30;
+      int A[4]= {12, 12, h, h};
+      int O[4] = {0, 0, h -10, -h + 10};
+      int phi = 1 *90;
+      double phase_diff[4] = {DEG2RAD(0), DEG2RAD(180), DEG2RAD(-90*1), DEG2RAD(90*1)};
+      Norn.execution(A,O,1000,phase_diff, 2);
       break;
   }
 
